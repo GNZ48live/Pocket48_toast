e6c91f6604766fb1313bd345f3690da45739d195
GNZ48live <40411768+GNZ48live@users.noreply.github.com>
2020-12-17 03:46:55 -0500

    Add files via upload
    Adjustable

--- PtoA.py
+++ PtoA.py
@@ -8,10 +8,15 @@ import argparse
 timestamps = []
 user = []
 info = []
-#Danmu position Y axis
-y_axis_list = [790,753,716,679,642] 
+#Setting
+
+font_size = 20        #font_size = 20
+line_spacing = 38     #line_spacing = 38
+line_num = 6          #line_num = 6
+default_y_axis = 780  #default_y_axis = 790
+
 #ass字幕文件头及样式
-Def_info = textwrap.dedent('''
+Def_info = f'''
 [Script Info]
 Title: Default ASS file
 ScriptType: v4.00+
@@ -24,13 +29,15 @@ Video Zoom Percent: 1
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: Default,微软雅黑,19,&H00F0F0F0,&H00FFFFFF,&H4C533B3B,&H910E0807,0,0,0,0,100.0,100.0,0.0,0.0,3,6.1923075,2.5,1,20,275,27,1
-Style: D2,微软雅黑,19,&H00FFCF9C,&H00FFFFFF,&H4C533B3B,&H910E0807,0,0,0,0,100.0,100.0,0.0,0.0,3,6.1923075,2.5,1,74,275,27,1
+Style: Default,微软雅黑,{font_size},&H00F0F0F0,&H00FFFFFF,&H4C533B3B,&H910E0807,0,0,0,0,100.0,100.0,0.0,0.0,3,6.1923075,2.5,1,20,275,27,1
+Style: D2,微软雅黑,{font_size},&H00FFCF9C,&H00FFFFFF,&H4C533B3B,&H910E0807,0,0,0,0,100.0,100.0,0.0,0.0,3,6.1923075,2.5,1,74,275,27,1
 
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
-''')
-
+'''
+def spacing_calculator():
+    y_axis_list = [default_y_axis-spacing*line_spacing for spacing in range(line_num)]
+    return y_axis_list
 def main():
     parser = argparse.ArgumentParser()
     add = parser.add_argument
@@ -40,15 +47,16 @@ def main():
     
     file_name = args.lrc_file.split('.')
     file_name = file_name[0]
+    y_axis_list = spacing_calculator()
 
     with open(args.lrc_file,encoding='utf-8') as fin:
         with open(f'{file_name}.ass','w',encoding='utf-8') as fout:
             i = 1
-            #Separate user name and the info
+            #用户与信息分离
             for line in fin:
                 try:
                     line = re.split(r']|\t',line)
-                    line_time = line[0].replace('[','')
+                    line_time = line[0]
                     timestamps.append(line_time[1:-1]) 
                     user.append(line[1])
                     info.append(' ' + line[2].replace('\n',''))
@@ -64,18 +72,19 @@ def main():
             t = '\\r'
             b = 1
             for user_,info_ in zip(user,info):
-                for n,y_axis in zip(range(1,6),y_axis_list):
+                for n,y_axis in zip(range(1,line_num+1),y_axis_list):
                     time_start_num = n + b - 2
                     time_end_num = n + b - 1
                     try:
                         time_star = timestamps[time_start_num]
                         time_end = timestamps[time_end_num]
-                    except BaseException:
-                        break
+                    except:
+                        pass
                     else:
                         Dialogue = f'Dialogue: 4,{time_star},{time_end},Default,,0,0,0,,{{\pos(20,{y_axis})}}{{{t}D2}}{user_}:{{{t}}}{info_}'
                         fout.write(Dialogue + '\n')           
                 b += 1
+                
 
 if __name__ == '__main__':
     main()
