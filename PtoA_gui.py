d0e6ff5c8a9f028de8e3601f1840916ed710fc4f
GNZ48live <40411768+GNZ48live@users.noreply.github.com>
2022-12-26 22:07:08 -0500

    增加可调起始位置参数

--- PtoA_gui.py
+++ PtoA_gui.py
@@ -1,4 +1,5 @@
-from tkinter import *
+#from tkinter import *
+from ttkbootstrap import *
 import tkinter.messagebox as messagebox
 import tkinter.filedialog
 import os
@@ -40,13 +41,13 @@ class Application(Frame):
         self.nameInput4.grid(row=2,column=4)
         self.label5.grid(row=3,column=3)
         self.nameInput5.grid(row=3,column=4)
-        self.transButton2.grid(row=2,column=0)
-        self.checkbox.grid(row=2,column=1)
+        self.transButton2.grid(row=3,column=0)
+        self.checkbox.grid(row=3,column=1)
 
         self.nameInput2.insert(0, 20)
-        self.nameInput3.insert(0, 38)
+        self.nameInput3.insert(0, 35)
         self.nameInput4.insert(0, 6)
-        self.nameInput5.insert(0, 790)
+        self.nameInput5.insert(0, 800)
 
     def filepath_fetch(self):
         self.nameInput1.delete(0,'end')
@@ -59,10 +60,10 @@ class Application(Frame):
         info = []
         #Setting
         try:
-            font_size = int(self.nameInput2.get())        #font_size = 20
-            line_spacing = int(self.nameInput3.get())   #line_spacing = 38
+            font_size = int(self.nameInput2.get())        #font_size = 22
+            line_spacing = int(self.nameInput3.get())   #line_spacing = 35
             line_num = int(self.nameInput4.get())        #line_num = 6
-            default_y_axis = int(self.nameInput5.get())  #default_y_axis = 790
+            default_y_axis = int(self.nameInput5.get())  #default_y_axis = 800
         except:
             return messagebox.showinfo('Caution!', '请确保参数正确')
 
@@ -80,8 +81,8 @@ Video Zoom Percent: 1
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: Default,微软雅黑,{font_size},&H00F0F0F0,&H00FFFFFF,&H4C533B3B,&H910E0807,0,0,0,0,100.0,100.0,0.0,0.0,3,6.1923075,2.5,1,20,275,27,1
-Style: D2,微软雅黑,{font_size},&H00FFCF9C,&H00FFFFFF,&H4C533B3B,&H910E0807,0,0,0,0,100.0,100.0,0.0,0.0,3,6.1923075,2.5,1,74,275,27,1
+Style: Default,微软雅黑,{font_size},&H00F6FCFF,&H00FFFFFF,&H88000000,&H910E0807,0,0,0,0,100.0,100.0,0.0,0.0,3,6.1923075,0,1,20,275,27,1
+Style: D2,微软雅黑,{font_size},&H00F6FCFF,&H00FFFFFF,&H88000000,&H910E0807,0,0,0,0,100.0,100.0,0.0,0.0,3,6.1923075,0,1,74,275,27,1
 
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
@@ -101,12 +102,13 @@ Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
                 #用户与信息分离
                 for line in fin:
                     try:
-                        line = re.split(r']|\t',line)
-                        line_time = line[0]
-                        timestamps.append(line_time[1:-1]) 
-                        user.append(line[1])
-                        info.append(' ' + line[2].replace('\n',''))
-                        i += 1
+                        if ']' in line:   
+                           line = re.split(r']|\t',line)
+                           line_time = line[0]
+                           timestamps.append(line_time[1:-1]) 
+                           user.append(line[1])
+                           info.append(' ' + line[2].replace('\n',''))
+                           i += 1
                     except:
                         error_logging = f'第{i}行 内容： {line} 错误'
                         i += 1
@@ -127,10 +129,10 @@ Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
                             pass
                         else:
                             if self.var1.get() == 0:
-                                Dialogue = f'Dialogue: 4,{time_star},{time_end},Default,,0,0,0,,{{\pos(20,{y_axis})}}{info_}'
+                                Dialogue = f'Dialogue: 4,{time_star},{time_end},Default,,0,0,0,,{{\pos(10,{y_axis})}}{info_}'
                                 fout.write(Dialogue + '\n')
                             else:
-                                Dialogue = f'Dialogue: 4,{time_star},{time_end},Default,,0,0,0,,{{\pos(20,{y_axis})}}{{{t}D2}}{user_}:{{{t}}}{info_}'
+                                Dialogue = f'Dialogue: 4,{time_star},{time_end},Default,,0,0,0,,{{\pos(10,{y_axis})}}{{{t}D2}}{user_}:{info_}'
                                 fout.write(Dialogue + '\n')
 
                     b += 1
@@ -140,6 +142,7 @@ Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
 app = Application()
 # 设置窗口标题:
 app.master.title('Pocket48 lrc to ass')
-app.master.geometry('500x120')
+app.master.geometry('500x140')
+app.master.resizable(False,False)
 # 主消息循环:
 app.mainloop()
